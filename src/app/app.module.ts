a3668243427e225c2bfe44bf9fe5ee08ee70fefd
Tuomas Juopperi <tuomaju@ety-a2123b-30.edu.wks.metropolia.fi>
2018-01-16 11:41:45 +0200

    valmis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    MainContentComponent,
+    SidebarComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
